b03157629c4ad9d67238d250608680eae260d751
GABRIEL FERREIRA ANGELO <gabriel.feang@gmail.com>
2026-02-07 14:16:16 -0300

    Simplify workflow logic. Remove redundant push and workflow_dispatch event checks. Default to building and deploying for cron events, aligning with expected behavior.

--- check_cron_or_pr.sh
+++ check_cron_or_pr.sh
@@ -8,25 +8,10 @@ if [[ "${GITHUB_EVENT_NAME}" == "pull_request" ]]; then
 
   export SHOULD_BUILD="yes"
 	export SHOULD_DEPLOY="no"
-elif [[ "${GITHUB_EVENT_NAME}" == "push" ]]; then
-	echo "It's a Push"
+else
+	echo "It's a ${GITHUB_EVENT_NAME}"
 
 	export SHOULD_BUILD="yes"
-	export SHOULD_DEPLOY="no"
-elif [[ "${GITHUB_EVENT_NAME}" == "workflow_dispatch" ]]; then
-  if [[ "${GENERATE_ASSETS}" == "true" ]]; then
-    echo "It will generate the assets"
-
-    export SHOULD_BUILD="yes"
-    export SHOULD_DEPLOY="no"
-  else
-  	echo "It's a Dispatch"
-
-    export SHOULD_DEPLOY="yes"
-  fi
-else
-	echo "It's a Cron"
-
 	export SHOULD_DEPLOY="yes"
 fi
 
